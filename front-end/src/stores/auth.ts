aa99b2e1434c93d904f60431c4a80bc83032f033
Kirial-Adin <kirial.adin@yandex.ru>
2025-04-11 22:08:05 +0300

    Part of the front-end logic

---
+++ front-end/src/stores/auth.ts
@@ -0,0 +1,33 @@
+import { ref } from 'vue'
+import { defineStore } from 'pinia'
+import { router } from '@/main'
+import type { IUser } from '../models/IUser'
+import { API_URL } from '../http/api'
+import type { AuthResponse } from '../models/response/AuthResponse'
+import axios from 'axios'
+import AuthService from '../services/AuthService'
+
+
+
+
+export const useAuthStore = defineStore('auth', () => {
+    const userInfo = ref({
+        user: {} as IUser,
+        isAuth: false,
+      })
+    const isAuth = ref(false)
+    const registration = async (email: string, password: string, weight: number, height: number, experience: string, trainingGoal: string) => {
+      try {
+        const response = await AuthService.registration(email, password, weight, height, experience, trainingGoal)
+        console.log(response.data)
+        localStorage.setItem('token', response.data.accessToken)
+        userInfo.value = {
+          user: response.data.user,
+          isAuth: true,
+        }
+        router.replace('/')
+      } catch (e: any) {
+        alert(e.response.data.message)
+      }
+    }
+})
